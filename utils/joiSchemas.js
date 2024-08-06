a3e8adeb6cb932e7486d46bca56cd0bf4687cda9
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2024-08-06 09:25:24 +0600

    fix minor issues

--- utils/joiSchemas.js
+++ utils/joiSchemas.js
@@ -1,21 +1,21 @@
 const baseJoi = require("joi");
-const sanitizeHTML = require("sanitize-html");
+const sanitizeHtml = require("sanitize-html");
 
 const extension = (joi) => ({
   type: "string",
   base: joi.string(),
   messages: {
-    "string.escapeHTML": "{{#label}} must not include HTML!",
+    "string.escapeHtml": "{{#label}} must not include HTML!",
   },
   rules: {
-    escapeHTML: {
+    escapeHtml: {
       validate(value, helpers) {
         const clean = sanitizeHtml(value, {
           allowedTags: [],
           allowedAttributes: {},
         });
         if (clean !== value)
-          return helpers.error("string.escapeHTML", { value });
+          return helpers.error("string.escapeHtml", { value });
         return clean;
       },
     },
@@ -24,19 +24,19 @@ const extension = (joi) => ({
 const Joi = baseJoi.extend(extension);
 
 const campJoiSchema = Joi.object({
-  title: Joi.string().required().escapeHTML(),
-  location: Joi.string().required().escapeHTML(),
+  title: Joi.string().required().escapeHtml(),
+  location: Joi.string().required().escapeHtml(),
   price: Joi.number().required().min(1),
   campImage: Joi.object({
-    url: Joi.string(),
-    filename: Joi.string(),
+    url: Joi.string().escapeHtml(),
+    filename: Joi.string().escapeHtml(),
   }),
-  amenities: Joi.string().escapeHTML(),
+  amenities: Joi.string().escapeHtml(),
   capacity: Joi.number().required().min(1),
-  area: Joi.string().required().escapeHTML(),
-  description: Joi.string().required().escapeHTML(),
-  contact: Joi.string().required().escapeHTML(),
-  website: Joi.string().escapeHTML(),
+  area: Joi.string().required().escapeHtml(),
+  description: Joi.string().required().escapeHtml(),
+  contact: Joi.string().required().escapeHtml(),
+  website: Joi.string().escapeHtml(),
   "g-recaptcha-response": Joi.string().required().messages({
     "string.empty": "Complete Captcha to proceed!",
     "any.required": "Captcha is required",
@@ -44,7 +44,7 @@ const campJoiSchema = Joi.object({
 }).required();
 
 const reviewJoiSchema = Joi.object({
-  content: Joi.string().required().min(5).escapeHTML(),
+  content: Joi.string().required().min(5).escapeHtml(),
   rating: Joi.number().required().min(1),
   "g-recaptcha-response": Joi.string().required().messages({
     "string.empty": "Complete Captcha to proceed!",
@@ -53,14 +53,14 @@ const reviewJoiSchema = Joi.object({
 }).required();
 
 const userJoiSchema = Joi.object({
-  firstname: Joi.string().required().escapeHTML(),
-  lastname: Joi.string().required().escapeHTML(),
-  username: Joi.string().required().escapeHTML(),
-  password: Joi.string().required().escapeHTML(),
-  email: Joi.string().required().escapeHTML(),
-  userImage: Joi.string().escapeHTML(),
+  firstname: Joi.string().required().escapeHtml(),
+  lastname: Joi.string().required().escapeHtml(),
+  username: Joi.string().required().escapeHtml(),
+  password: Joi.string().required().escapeHtml(),
+  email: Joi.string().required().escapeHtml(),
+  userImage: Joi.string().escapeHtml(),
   birthDate: Joi.date().required(),
-  about: Joi.string().escapeHTML(),
+  about: Joi.string().escapeHtml(),
   isAdmin: Joi.boolean(),
   "g-recaptcha-response": Joi.string().required().messages({
     "string.empty": "Complete Captcha to proceed!",
