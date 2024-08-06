e4eedb5ec4892762d00e1c683b4f3bafbbe39250
Md. Shariul Islam <26851594+shariult@users.noreply.github.com>
2024-08-06 07:34:17 +0600

    fix minor issue

--- middleware/joiValidate.js
+++ middleware/joiValidate.js
@@ -1,10 +1,14 @@
 const joiSchemas = require("../utils/joiSchemas");
 const errorHandler = require("../utils/errorHandler");
+const { cloudinary } = require("../utils/cloudinaryConfig");
 
 function validateCamp(req, res, next) {
   const validate = joiSchemas.campJoiSchema.validate(req.body);
   if (validate.error) {
     const message = validate.error.details.map((err) => err.message).join(",");
+    if (req.method === "POST") {
+      cloudinary.uploader.destroy(req.file.filename);
+    }
     throw new errorHandler.ExpressError(message, 400);
   } else {
     next();
